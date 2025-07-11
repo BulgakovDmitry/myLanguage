753e654898b26e0ad110179ba53119eebb4aea1e
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-07-11 04:08:31 +0300

    contributed new keywords to match my new grammar

--- frontend/headers/lexicalAnalysis.hpp
+++ frontend/headers/lexicalAnalysis.hpp
@@ -22,25 +22,26 @@ const char* const KEY_BUDET               = "budet";         // = (2)
 const char* const KEY_PRIBUDET            = "pribudet";      // var def
 const char* const KEY_GLAGOLI             = "glagoli";       // printf (1)
 const char* const KEY_YASNO               = "yasno";         // printf (2)
-const char* const KEY_SQRT                = "sqrt";
-const char* const KEY_SIN                 = "sin";
-const char* const KEY_COS                 = "cos";
-const char* const KEY_TG                  = "tg";
-const char* const KEY_LN                  = "ln";
-const char* const KEY_ADD                 = "+";
-const char* const KEY_SUB                 = "-";
-const char* const KEY_MUL                 = "*";
-const char* const KEY_DIV                 = "/";
-const char* const KEY_equal               = "==";                  
-const char* const KEY_GREATER             = ">";                
-const char* const KEY_LESS                = "<";                
-const char* const KEY_GREATER_OR_EQUAL    = ">=";  
-const char* const KEY_LESS_OR_EQUAL       = "<=";                 
+const char* const KEY_SQRT                = "sqrt";          // sqrt
+const char* const KEY_SIN                 = "sin";           // sin
+const char* const KEY_COS                 = "cos";           // cos
+const char* const KEY_TG                  = "tg";            // tg
+const char* const KEY_LN                  = "ln";            // ln
+const char* const KEY_ADD                 = "+";             // +
+const char* const KEY_SUB                 = "-";             // -
+const char* const KEY_MUL                 = "*";             // *
+const char* const KEY_DIV                 = "/";             // /
+const char* const KEY_EQUAL               = "==";            // ==   
+const char* const KEY_NOT_EQUAL           = "!=";            // !=   
+const char* const KEY_GREATER             = ">";             // >
+const char* const KEY_LESS                = "<";             // <
+const char* const KEY_GREATER_OR_EQUAL    = ">=";            // >=
+const char* const KEY_LESS_OR_EQUAL       = "<=";            // <=
 const char* const KEY_AMIN                = "amin";          // hlt
-const char* const KEY_LEFT_PARENTHESIS    = "(";
-const char* const KEY_RIGHT_PARENTHESIS   = ")";
-const char* const KEY_LEFT_CURLY_BRACKET  = "{";
-const char* const KEY_RIGHT_CURLY_BRACKET = "}";
+const char* const KEY_LEFT_PARENTHESIS    = "(";             // (
+const char* const KEY_RIGHT_PARENTHESIS   = ")";             // )
+const char* const KEY_LEFT_CURLY_BRACKET  = "{";             // {
+const char* const KEY_RIGHT_CURLY_BRACKET = "}";             // }
 
 Vector splitIntoTokens(FILE* codeFile);
 void   tokenDump      (const Vector tokens);
